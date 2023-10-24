396559f91fea18364c2b3f37e591d88c141b9c0b
ItsLever <106493116+ItsLever@users.noreply.github.com>
2023-10-24 00:39:46 -0400

    Removed security flaw and changed token
    You tried.

--- src/main/java/me/lever/ipcbot/Hidden.java
+++ src/main/java/me/lever/ipcbot/Hidden.java
@@ -2,5 +2,5 @@ package me.lever.ipcbot;
 
 public class Hidden
 {
-    public static String BOTTOKENMAIN = "MTA2ODM3NDAxNTUzMTU2OTE3Mg.GRQeqX.emqr09W2wng_YMnoOzHVtDYZB4tiPUWr_74VSQ";
+    public static String BOTTOKENMAIN = "You Tried";
 }
